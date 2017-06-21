5e3e23123769bcbfd8080cbf54e98d8edd69c2ff
BlackZIjian <735809461@qq.com>
2017-06-21 11:49:34 +0800

    no message

---
+++ NET/DAO/EntityManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Net.MEntity;
+using MysqlOp;
+using MySql.Data.MySqlClient;
+
+
+namespace Net.DAO
+{
+    public class EntityManager
+    {
+        private static EntityManager mInstance;
+        public EntityManager Instance
+        {
+            get
+            {
+                if (mInstance != null)
+                {
+                    return mInstance;
+                }
+                else
+                {
+                    mInstance = new EntityManager();
+                    return mInstance;
+                }
+            }
+        }
+
+
+        public virtual Entity GetById(int id)
+        {
+            return null;
+        }
+
+        public virtual Entity Create(Dictionary<string,object> attr)
+        {
+            return null;
+        }
+
+        public virtual void Del(int id)
+        {
+            
+        }
+
+        public virtual Entity Update(int id,Dictionary<string,object> attr)
+        {
+            return null;
+        }
+    }
+}
---
+++ NET/Net/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Net.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
